0d1bd8c49e9129589fb041e84387c72682c13e50
hwelsters <hwelsters>
2024-08-01 23:35:05 -0700

    binit.

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
-curl https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth -o sam_vit_b_01ec64.pth
+mkdir model_checkpoints
+curl https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth -o model_checkpoints/sam_vit_b_01ec64.pth
