7f7da68f72ce7ded6605e2d46c9710eeae0b5609
hwelsters <hwelsters>
2024-08-01 23:37:18 -0700

    binit.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 mkdir model_checkpoints
 curl https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth -o model_checkpoints/sam_vit_b_01ec64.pth
+
+mkdir output
+mkdir output/ground
+mkdir output/predictions
