dc229bddce04ef45988771b7a638127294e5d2c4
hwelsters <hwelsters>
2024-08-02 03:02:06 -0700

    binit.

--- evaluate.py
+++ evaluate.py
@@ -90,7 +90,7 @@ def get_bbbox(ground_truth_map: np.array) -> list:
   return bbox
 
 def get_bounding_box(image, variable):
-    if np.sum(image) <= image.shape[0] * image.shape[1] * 0.05: return [random.random(), random.random(), random.random(), random.random()]
+    if np.sum(image) <= 1500: return [random.random(), random.random(), random.random(), random.random()]
 
     bbbox = get_bbbox(image)
     min_x = bbbox[0]    
